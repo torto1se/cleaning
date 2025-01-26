ea30bb26976e61c0ccb4800056962b5092032a94
tortoise <akvvsjcvdhi@gmail.com>
2025-01-26 22:23:26 +0300

    up

--- frontend/src/components/OrderHistory.jsx
+++ frontend/src/components/OrderHistory.jsx
@@ -1,159 +1,312 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate, Link } from 'react-router-dom';
-import ErrorMessage from './ErrorMessage';
+import React, { useEffect, useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+import ErrorMessage from './ErrorMessage'
 
 export default function OrderHistory() {
-    const [orders, setOrders] = useState([]);
-    const [error, setError] = useState('');
-    const navigate = useNavigate(); // Хук для навигации
+	const [orders, setOrders] = useState([])
+	const [error, setError] = useState('')
+	const navigate = useNavigate() // Хук для навигации
 
-    // Состояние для хранения выбранного статуса для каждого заказа
-    const [selectedStatuses, setSelectedStatuses] = useState({});
-    const [cancellationReasons, setCancellationReasons] = useState({}); // Состояние для хранения причин отмены
-    
-    // Проверяем, является ли пользователь администратором
-    const isAdmin = () => {
-        const token = localStorage.getItem('token');
-        if (!token) return false; 
-        const decodedToken = JSON.parse(atob(token.split('.')[1]));
-        return decodedToken.username === 'adminka'; 
-    };
+	// Состояние для хранения выбранного статуса для каждого заказа
+	const [selectedStatuses, setSelectedStatuses] = useState({})
+	const [cancellationReasons, setCancellationReasons] = useState({}) // Состояние для хранения причин отмены
 
-    // Проверка на наличие токена и определение роли
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (!token) {
-            navigate('/login'); // Перенаправление на страницу входа
-        } else {
-            fetchOrders(token); // Загружаем заказы только если токен есть
-        }
-    }, [navigate]);
+	// Проверяем, является ли пользователь администратором
+	const isAdmin = () => {
+		const token = localStorage.getItem('token')
+		if (!token) return false
+		const decodedToken = JSON.parse(atob(token.split('.')[1]))
+		return decodedToken.username === 'adminka'
+	}
 
-    const fetchOrders = async (token) => {
-        const response = await fetch('http://localhost:3001/orders', {
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        });
+	// Проверка на наличие токена и определение роли
+	useEffect(() => {
+		const token = localStorage.getItem('token')
+		if (!token) {
+			navigate('/login') // Перенаправление на страницу входа
+		} else {
+			fetchOrders(token) // Загружаем заказы только если токен есть
+		}
+	}, [navigate])
 
-        if (response.ok) {
-            const data = await response.json();
-            setOrders(data);
-            setError('');
-            const initialStatuses = {};
-            data.forEach(order => {
-                initialStatuses[order.id] = ''; // Устанавливаем пустое значение по умолчанию
-                setCancellationReasons(prev => ({ ...prev, [order.id]: '' })); // Инициализируем причины отмены
-            });
-            setSelectedStatuses(initialStatuses);
-        } else {
-            const data = await response.json();
-            setError(data.message);
-            setOrders([]);
-        }
-    };
+	const fetchOrders = async token => {
+		const response = await fetch('http://localhost:3001/orders', {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
 
-    const handleStatusChange = (orderId, newStatus) => {
-        setSelectedStatuses(prev => ({
-            ...prev,
-            [orderId]: newStatus,
-        }));
-        
-        // Если статус "Отменен", показываем поле для причины отмены
-        if (newStatus === "Отменен") {
-            setCancellationReasons(prev => ({ ...prev, [orderId]: '' })); // Сбрасываем причину отмены при изменении статуса
-        }
-    };
+		if (response.ok) {
+			const data = await response.json()
+			setOrders(data)
+			setError('')
+			const initialStatuses = {}
+			data.forEach(order => {
+				initialStatuses[order.id] = '' // Устанавливаем пустое значение по умолчанию
+				setCancellationReasons(prev => ({ ...prev, [order.id]: '' })) // Инициализируем причины отмены
+			})
+			setSelectedStatuses(initialStatuses)
+		} else {
+			const data = await response.json()
+			setError(data.message)
+			setOrders([])
+		}
+	}
 
-    const updateStatus = async (orderId) => {
-        const token = localStorage.getItem('token');
-        
-        const response = await fetch(`http://localhost:3001/orders/${orderId}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify({ 
-                status: selectedStatuses[orderId], 
-                cancellationReason: cancellationReasons[orderId] 
-            }),
-        });
+	const handleStatusChange = (orderId, newStatus) => {
+		setSelectedStatuses(prev => ({
+			...prev,
+			[orderId]: newStatus,
+		}))
 
-        if (response.ok) {
-            fetchOrders(token);
-        } else {
-            const data = await response.json();
-            setError(data.message);
-        }
-    };
+		// Если статус "Отменен", показываем поле для причины отмены
+		if (newStatus === 'Отменен') {
+			setCancellationReasons(prev => ({ ...prev, [orderId]: '' })) // Сбрасываем причину отмены при изменении статуса
+		}
+	}
 
-    return (
-        <div>
-            <ErrorMessage message={error} />
-            <h3>История заказов</h3>
-            {orders.length > 0 ? (
-                <table style={{ borderCollapse: 'collapse', width: '100%' }}>
-                    <thead>
-                        <tr>
-                            <th style={{ border: '1px solid black', padding: '8px' }}>ФИО</th>
-                            <th style={{ border: '1px solid black', padding: '8px' }}>Услуга</th>
-                            <th style={{ border: '1px solid black', padding: '8px' }}>Адрес</th>
-                            <th style={{ border: '1px solid black', padding: '8px' }}>Контакт</th>
-                            <th style={{ border: '1px solid black', padding: '8px' }}>Дата</th>
-                            <th style={{ border: '1px solid black', padding: '8px' }}>Время</th>
-                            <th style={{ border: '1px solid black', padding: '8px' }}>Способ оплаты</th>
-                            <th style={{ border: '1px solid black', padding: '8px' }}>Статус</th>
-                            {isAdmin() && <th style={{ border: '1px solid black', padding: '8px' }}>Действия</th>}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {orders.map(order => (
-                            <tr key={order.id}>
-                                <td style={{ border: '1px solid black', padding: '8px' }}>{order.fullname}</td>
-                                <td style={{ border: '1px solid black', padding: '8px' }}>{order.service}</td>
-                                <td style={{ border: '1px solid black', padding: '8px' }}>{order.address}</td>
-                                <td style={{ border: '1px solid black', padding: '8px' }}>{order.contact}</td>
-                                <td style={{ border: '1px solid black', padding: '8px' }}>{order.date}</td>
-                                <td style={{ border: '1px solid black', padding: '8px' }}>{order.time}</td>
-                                <td style={{ border: '1px solid black', padding: '8px' }}>{order.paymentMethod}</td>
-                                <td style={{ border: '1px solid black', padding: '8px' }}>
-                                    {order.status} 
-                                    {order.status === "Отменен" && order.cancellationReason && ` - Причина: ${order.cancellationReason}`}
-                                </td>
-                                {isAdmin() && (
-                                    <td style={{ border: '1px solid black', padding: '8px' }}>
-                                        <select 
-                                            value={selectedStatuses[order.id] || ''} 
-                                            onChange={(e) => handleStatusChange(order.id, e.target.value)} 
-                                        >
-                                            <option value="" disabled>Выберите статус</option>
-                                            <option value="В обработке">В обработке</option>
-                                            <option value="Завершен">Завершен</option>
-                                            <option value="Отменен">Отменен</option>
-                                        </select>
-                                        {selectedStatuses[order.id] === "Отменен" && (
-                                            <>
-                                                <input 
-                                                    type="text" 
-                                                    placeholder="Причина отмены" 
-                                                    value={cancellationReasons[order.id]} 
-                                                    onChange={(e) => setCancellationReasons(prev => ({ ...prev, [order.id]: e.target.value }))} 
-                                                />
-                                            </>
-                                        )}
+	const updateStatus = async orderId => {
+		const token = localStorage.getItem('token')
 
-                                        <button onClick={() => updateStatus(order.id)}>Обновить статус</button>
-                                    </td>
-                                )}
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            ) : (
-                <p>Нет заказов.</p>
-            )}
-           <Link to={'/order'}>Создать заявку</Link> 
-        </div>
-    );
+		const response = await fetch(`http://localhost:3001/orders/${orderId}`, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+			body: JSON.stringify({
+				status: selectedStatuses[orderId],
+				cancellationReason: cancellationReasons[orderId],
+			}),
+		})
+
+		if (response.ok) {
+			fetchOrders(token)
+		} else {
+			const data = await response.json()
+			setError(data.message)
+		}
+	}
+
+	return (
+		<div
+			style={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+				padding: '20px',
+				borderRadius: '10px',
+				boxShadow: '0 0 10px rgba(0,0,0,0.1)',
+			}}
+		>
+			<ErrorMessage message={error} />
+			<h3 style={{ marginBottom: '20px' }}>История заказов</h3>
+			{orders.length > 0 ? (
+				<table
+					style={{
+						borderCollapse: 'collapse',
+						width: '100%',
+						backgroundColor: 'white',
+						boxShadow: '0 0 10px rgba(0,0,0,0.1)',
+						borderRadius: '5px',
+					}}
+				>
+					<thead>
+						<tr>
+							<th
+								style={{
+									border: '1px solid #ddd',
+									padding: '8px',
+									backgroundColor: '#f5f5f5',
+								}}
+							>
+								ФИО
+							</th>
+							<th
+								style={{
+									border: '1px solid #ddd',
+									padding: '8px',
+									backgroundColor: '#f5f5f5',
+								}}
+							>
+								Услуга
+							</th>
+							<th
+								style={{
+									border: '1px solid #ddd',
+									padding: '8px',
+									backgroundColor: '#f5f5f5',
+								}}
+							>
+								Адрес
+							</th>
+							<th
+								style={{
+									border: '1px solid #ddd',
+									padding: '8px',
+									backgroundColor: '#f5f5f5',
+								}}
+							>
+								Контакт
+							</th>
+							<th
+								style={{
+									border: '1px solid #ddd',
+									padding: '8px',
+									backgroundColor: '#f5f5f5',
+								}}
+							>
+								Дата
+							</th>
+							<th
+								style={{
+									border: '1px solid #ddd',
+									padding: '8px',
+									backgroundColor: '#f5f5f5',
+								}}
+							>
+								Время
+							</th>
+							<th
+								style={{
+									border: '1px solid #ddd',
+									padding: '8px',
+									backgroundColor: '#f5f5f5',
+								}}
+							>
+								Способ оплаты
+							</th>
+							<th
+								style={{
+									border: '1px solid #ddd',
+									padding: '8px',
+									backgroundColor: '#f5f5f5',
+								}}
+							>
+								Статус
+							</th>
+							{isAdmin() && (
+								<th
+									style={{
+										border: '1px solid #ddd',
+										padding: '8px',
+										backgroundColor: '#f5f5f5',
+									}}
+								>
+									Действия
+								</th>
+							)}
+						</tr>
+					</thead>
+					<tbody>
+						{orders.map(order => (
+							<tr key={order.id}>
+								<td style={{ border: '1px solid #ddd', padding: '8px' }}>
+									{order.fullname}
+								</td>
+								<td style={{ border: '1px solid #ddd', padding: '8px' }}>
+									{order.service}
+								</td>
+								<td style={{ border: '1px solid #ddd', padding: '8px' }}>
+									{order.address}
+								</td>
+								<td style={{ border: '1px solid #ddd', padding: '8px' }}>
+									{order.contact}
+								</td>
+								<td style={{ border: '1px solid #ddd', padding: '8px' }}>
+									{order.date}
+								</td>
+								<td style={{ border: '1px solid #ddd', padding: '8px' }}>
+									{order.time}
+								</td>
+								<td style={{ border: '1px solid #ddd', padding: '8px' }}>
+									{order.paymentMethod}
+								</td>
+								<td style={{ border: '1px solid #ddd', padding: '8px' }}>
+									{order.status}
+									{order.status === 'Отменен' &&
+										order.cancellationReason &&
+										` - Причина: ${order.cancellationReason}`}
+								</td>
+								{isAdmin() && (
+									<td style={{ border: '1px solid #ddd', padding: '8px' }}>
+										<select
+											value={selectedStatuses[order.id] || ''}
+											onChange={e =>
+												handleStatusChange(order.id, e.target.value)
+											}
+											style={{
+												padding: '5px',
+												width: '100%',
+												maxWidth: '150px',
+												borderRadius: '5px',
+												border: '1px solid #ccc',
+											}}
+										>
+											<option value='' disabled>
+												Выберите статус
+											</option>
+											<option value='В обработке'>В обработке</option>
+											<option value='Завершен'>Завершен</option>
+											<option value='Отменен'>Отменен</option>
+										</select>
+										{selectedStatuses[order.id] === 'Отменен' && (
+											<>
+												<input
+													type='text'
+													placeholder='Причина отмены'
+													value={cancellationReasons[order.id]}
+													onChange={e =>
+														setCancellationReasons(prev => ({
+															...prev,
+															[order.id]: e.target.value,
+														}))
+													}
+													style={{
+														padding: '5px',
+														width: '100%',
+														maxWidth: '150px',
+														borderRadius: '5px',
+														border: '1px solid #ccc',
+														marginTop: '5px',
+													}}
+												/>
+											</>
+										)}
+
+										<button
+											onClick={() => updateStatus(order.id)}
+											style={{
+												padding: '5px 10px',
+												backgroundColor: '#007bff',
+												color: 'white',
+												border: 'none',
+												borderRadius: '5px',
+												cursor: 'pointer',
+												marginTop: '5px',
+											}}
+										>
+											Обновить статус
+										</button>
+									</td>
+								)}
+							</tr>
+						))}
+					</tbody>
+				</table>
+			) : (
+				<p style={{ color: 'gray' }}>Нет заказов.</p>
+			)}
+			<Link
+				to={'/order'}
+				style={{
+					textDecoration: 'none',
+					color: '#007bff',
+					marginTop: '10px',
+				}}
+			>
+				Создать заявку
+			</Link>
+		</div>
+	)
 }
--- frontend/src/components/OrderPage.jsx
+++ frontend/src/components/OrderPage.jsx
@@ -1,120 +1,214 @@
-import React, { useState } from "react";
-import { useNavigate, Link } from 'react-router-dom';
-import ErrorMessage from './ErrorMessage';
+import React, { useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+import ErrorMessage from './ErrorMessage'
 
 const services = [
-    "Общий клининг",
-    "Генеральная уборка",
-    "Послестроительная уборка",
-    "Химчистка ковров и мебели"
-];
+	'Общий клининг',
+	'Генеральная уборка',
+	'Послестроительная уборка',
+	'Химчистка ковров и мебели',
+]
 
-const paymentMethods = [
-    "Наличные",
-    "Банковская карта"
-];
+const paymentMethods = ['Наличные', 'Банковская карта']
 
 export default function OrderPage() {
-    const [service, setService] = useState('');
-    const [address, setAddress] = useState('');
-    const [contact, setContact] = useState('');
-    const [date, setDate] = useState('');
-    const [time, setTime] = useState('');
-    const [paymentMethod, setPaymentMethod] = useState('');
-    const [error, setError] = useState('');
-    const [success, setSuccess] = useState('');
+	const [service, setService] = useState('')
+	const [address, setAddress] = useState('')
+	const [contact, setContact] = useState('')
+	const [date, setDate] = useState('')
+	const [time, setTime] = useState('')
+	const [paymentMethod, setPaymentMethod] = useState('')
+	const [error, setError] = useState('')
+	const [success, setSuccess] = useState('')
 
-    const navigate = useNavigate();
+	const navigate = useNavigate()
 
-    // Проверка на наличие токена
-    React.useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (!token) {
-            navigate('/login'); // Перенаправление на страницу входа
-        }
-    }, [navigate]);
+	// Проверка на наличие токена
+	React.useEffect(() => {
+		const token = localStorage.getItem('token')
+		if (!token) {
+			navigate('/login') // Перенаправление на страницу входа
+		}
+	}, [navigate])
 
-    const handleOrderSubmit = async () => {
-        if (!service || !address || !contact || !date || !time || !paymentMethod) {
-            setError('Все поля должны быть заполнены');
-            return;
-        }
+	const handleOrderSubmit = async () => {
+		if (!service || !address || !contact || !date || !time || !paymentMethod) {
+			setError('Все поля должны быть заполнены')
+			return
+		}
 
-        const token = localStorage.getItem('token');
+		const token = localStorage.getItem('token')
 
-        const response = await fetch('http://localhost:3001/orders', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify({ service, address, contact, date, time, paymentMethod }),
-        });
+		const response = await fetch('http://localhost:3001/orders', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+			body: JSON.stringify({
+				service,
+				address,
+				contact,
+				date,
+				time,
+				paymentMethod,
+			}),
+		})
 
-        if (response.ok) {
-            setSuccess('Заказ успешно отправлен!');
-            setError('');
-            setService('');
-            setAddress('');
-            setContact('');
-            setDate('');
-            setTime('');
-            setPaymentMethod('');
-        } else {
-            const data = await response.json();
-            setError(data.message);
-        }
-    };
+		if (response.ok) {
+			setSuccess('Заказ успешно отправлен!')
+			setError('')
+			setService('')
+			setAddress('')
+			setContact('')
+			setDate('')
+			setTime('')
+			setPaymentMethod('')
+		} else {
+			const data = await response.json()
+			setError(data.message)
+		}
+	}
 
-    return (
-        <div>
-            <ErrorMessage message={error} />
-            {success && <div style={{ color: 'green' }}>{success}</div>}
-            <h3>Создать заказ</h3>
-            
-            <select value={service} onChange={(e) => setService(e.target.value)}>
-                <option value="">Выберите услугу</option>
-                {services.map((s) => (
-                    <option key={s} value={s}>{s}</option>
-                ))}
-            </select>
+	return (
+		<div
+			style={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+				padding: '20px',
+				borderRadius: '10px',
+				boxShadow: '0 0 10px rgba(0,0,0,0.1)',
+			}}
+		>
+			<ErrorMessage message={error} />
+			{success && (
+				<div style={{ color: 'green', marginBottom: '10px' }}>{success}</div>
+			)}
+			<h3 style={{ marginBottom: '20px' }}>Создать заявку</h3>
 
-            <input 
-                type="text" 
-                placeholder="Адрес" 
-                value={address} 
-                onChange={(e) => setAddress(e.target.value)} 
-            />
-            
-            <input 
-                type="text" 
-                placeholder="Контактные данные" 
-                value={contact} 
-                onChange={(e) => setContact(e.target.value)} 
-            />
-            
-            <input 
-                type="date" 
-                value={date} 
-                onChange={(e) => setDate(e.target.value)} 
-            />
-            
-            <input 
-                type="time" 
-                value={time} 
-                onChange={(e) => setTime(e.target.value)} 
-            />
+			<select
+				value={service}
+				onChange={e => setService(e.target.value)}
+				style={{
+					padding: '10px',
+					width: '100%',
+					maxWidth: '300px',
+					marginBottom: '15px',
+					borderRadius: '5px',
+					border: '1px solid #ccc',
+				}}
+			>
+				<option value=''>Выберите услугу</option>
+				{services.map(s => (
+					<option key={s} value={s}>
+						{s}
+					</option>
+				))}
+			</select>
 
-            <select value={paymentMethod} onChange={(e) => setPaymentMethod(e.target.value)}>
-                <option value="">Выберите тип оплаты</option>
-                {paymentMethods.map((method) => (
-                    <option key={method} value={method}>{method}</option>
-                ))}
-            </select>
+			<input
+				type='text'
+				placeholder='Адрес'
+				value={address}
+				onChange={e => setAddress(e.target.value)}
+				style={{
+					padding: '10px',
+					width: '100%',
+					maxWidth: '300px',
+					marginBottom: '15px',
+					borderRadius: '5px',
+					border: '1px solid #ccc',
+				}}
+			/>
 
-            <button onClick={handleOrderSubmit}>Отправить заказ</button>
-            <Link to='/order-history'>История заказов</Link>
-        </div>
-    );
+			<input
+				type='text'
+				placeholder='Контактные данные'
+				value={contact}
+				onChange={e => setContact(e.target.value)}
+				style={{
+					padding: '10px',
+					width: '100%',
+					maxWidth: '300px',
+					marginBottom: '15px',
+					borderRadius: '5px',
+					border: '1px solid #ccc',
+				}}
+			/>
+
+			<input
+				type='date'
+				value={date}
+				onChange={e => setDate(e.target.value)}
+				style={{
+					padding: '10px',
+					width: '100%',
+					maxWidth: '300px',
+					marginBottom: '15px',
+					borderRadius: '5px',
+					border: '1px solid #ccc',
+				}}
+			/>
+
+			<input
+				type='time'
+				value={time}
+				onChange={e => setTime(e.target.value)}
+				style={{
+					padding: '10px',
+					width: '100%',
+					maxWidth: '300px',
+					marginBottom: '15px',
+					borderRadius: '5px',
+					border: '1px solid #ccc',
+				}}
+			/>
+
+			<select
+				value={paymentMethod}
+				onChange={e => setPaymentMethod(e.target.value)}
+				style={{
+					padding: '10px',
+					width: '100%',
+					maxWidth: '300px',
+					marginBottom: '15px',
+					borderRadius: '5px',
+					border: '1px solid #ccc',
+				}}
+			>
+				<option value=''>Выберите тип оплаты</option>
+				{paymentMethods.map(method => (
+					<option key={method} value={method}>
+						{method}
+					</option>
+				))}
+			</select>
+
+			<button
+				onClick={handleOrderSubmit}
+				style={{
+					padding: '10px 20px',
+					backgroundColor: '#007bff',
+					color: 'white',
+					border: 'none',
+					borderRadius: '5px',
+					cursor: 'pointer',
+				}}
+			>
+				Отправить заказ
+			</button>
+
+			<Link
+				to='/order-history'
+				style={{
+					textDecoration: 'none',
+					color: '#007bff',
+					marginTop: '10px',
+				}}
+			>
+				История заказов
+			</Link>
+		</div>
+	)
 }
-
